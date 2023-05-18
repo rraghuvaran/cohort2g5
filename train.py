74484f02ed4bae128a1900e0205ce10a739c41dd
Rupesh Raghuvaran <rupesh.raghuvaran@nilesecure.com>
2023-05-19 01:49:52 +0530

    Add the transform to manage image and target bboxes appropriately.

--- train.py
+++ train.py
@@ -1,5 +1,10 @@
 import numpy as np
-from torchvision import transforms 
+import torch
+from torchvision import transforms
+from torchvision import datasets
+from torchvision.transforms import ToTensor
+import matplotlib.pyplot as plt
+
 # Applying Transforms to the Data
 image_transforms = { 
     'train': transforms.Compose([
@@ -30,24 +35,25 @@ image_transforms = {
 # Load the Data
 # Set train and valid directory paths
 train_directory = 'train'
-valid_directory = 'test'
+valid_directory = 'val'
+test_directory = 'test'
 # Batch size
 bs = 32
 # Number of classes
 num_classes = 10
 # Load Data from folders
 data = {
-    'train': datasets.ImageFolder(root=train_directory, transform=image_transforms['train']),
-    'valid': datasets.ImageFolder(root=valid_directory, transform=image_transforms['valid']),
-    'test': datasets.ImageFolder(root=test_directory, transform=image_transforms['test'])
+    'train': datasets.Unimib2016FoodDataset(root=train_directory, transform=image_transforms['train']),
+    'validation': datasets.Unimib2016FoodDataset(root=valid_directory, transform=image_transforms['valid']),
+    'test': datasets.Unimib2016FoodDataset(root=test_directory, transform=image_transforms['test'])
 }
 # Size of Data, to be used for calculating Average Loss and Accuracy
 train_data_size = len(data['train'])
-valid_data_size = len(data['valid'])
+validation_data_size = len(data['validation'])
 test_data_size = len(data['test'])
 # Create iterators for the Data loaded using DataLoader module
 train_data = DataLoader(data['train'], batch_size=bs, shuffle=True)
-valid_data = DataLoader(data['valid'], batch_size=bs, shuffle=True)
+valid_data = DataLoader(data['validation'], batch_size=bs, shuffle=True)
 test_data = DataLoader(data['test'], batch_size=bs, shuffle=True)
 # Print the train, validation and test set data sizes
-train_data_size, valid_data_size, test_data_size
+print(train_data_size, valid_data_size, test_data_size)
